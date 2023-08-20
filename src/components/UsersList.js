690e7ca0db08b6968a12b200e61c33e5524a9307
Pritam Gawade <pritam@7gen.com>
2023-08-19 18:07:33 -0700

    Added User and displayed list using Async Thunk

--- src/components/UsersList.js
+++ src/components/UsersList.js
@@ -1,8 +1,62 @@
-
+import { useEffect } from "react";
+import { useSelector } from 'react-redux';
+import { fetchUsers, addUser } from '../store';
+import useThunk from '../hooks/use-thunk';
+import Skeleton from "./Skeleton";
+import Button from './Button';
 
 function UsersList() {
+    const [doFetchUsers, isLoadingUsers, loadingUsersError] = useThunk(fetchUsers);
+
+    const [doCreateUser, isCreatingUser, creatingUserError] = useThunk(addUser);
+
+    const { data } = useSelector((state) => {
+        return state.users
+    })
+
+    useEffect(() => {
+        doFetchUsers();
+    }, [doFetchUsers]);
+
+    const handleUserAdd = () => {
+        doCreateUser();
+    }
+
+    let content;
+
+    if (isLoadingUsers) {
+        content = <Skeleton times={6} className="h-10 w-full" />;
+    } else if (loadingUsersError) {
+        content = <div>Error fetching data...</div>;
+    } else {
+        content = data.map((user) => {
+            return (
+                <div key={user.id} className="mb-2 border rounded">
+                    <div className="flex p-2 justify-between items-center cursor-pointer">
+                        {user.name}
+                    </div>
+                </div>
+            )
+        })
+    }
+
+    if (isLoadingUsers) {
+        return <Skeleton times={6} className="h-10 w-full" />
+    }
+
+    if (loadingUsersError) {
+        return <div>Error fetching data...</div>;
+    }
+
     return (
-        <div>Users List</div>
+        <div>
+            <div className="flex flex-row justify-between items-center m-3">
+                <h1 className="m-2 text-xl">Users</h1>
+                <Button loading={isCreatingUser} onClick={handleUserAdd}>+ Add User</Button>
+                {creatingUserError && 'Error creating user...'}
+            </div>
+            {content}
+        </div>
     );
 }
 
