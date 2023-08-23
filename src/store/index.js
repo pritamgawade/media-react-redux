041ea635f51056a571709c02258b2805157678d9
Pritam Gawade <pritam@7gen.com>
2023-08-23 10:58:42 -0700

    Add, fetch, delete photos from album

--- src/store/index.js
+++ src/store/index.js
@@ -2,16 +2,18 @@ import { configureStore } from "@reduxjs/toolkit";
 import { usersReducer } from './slices/usersSlice';
 import { setupListeners } from "@reduxjs/toolkit/query";
 import { albumsApi } from "./apis/albumApi";
-
+import { photosApi } from "./apis/photosApi";
 
 export const store = configureStore({
     reducer: {
         users: usersReducer,
-        [albumsApi.reducerPath]: albumsApi.reducer
+        [albumsApi.reducerPath]: albumsApi.reducer,
+        [photosApi.reducerPath]: photosApi.reducer
     },
     middleware: (getDefaultMiddleware) => {
         return getDefaultMiddleware()
-            .concat(albumsApi.middleware);
+            .concat(albumsApi.middleware)
+            .concat(photosApi.middleware)
     }
 });
 
@@ -20,4 +22,13 @@ setupListeners(store.dispatch);
 export * from './thunks/fetchUsers';
 export * from './thunks/addUser';
 export * from './thunks/removeUser';
-export { useFetchAlbumsQuery, useAddAlbumMutation, useRemoveAlbumMutation } from './apis/albumApi';
+export {
+    useFetchAlbumsQuery,
+    useAddAlbumMutation,
+    useRemoveAlbumMutation
+} from './apis/albumApi';
+export {
+    useFetchPhotosQuery,
+    useAddPhotoMutation,
+    useRemovePhotoMutation
+} from './apis/photosApi';
